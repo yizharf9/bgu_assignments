73cd8279d869dfb50c82717aa5d4d69133ef3588
yizhar fahima <yizharf9@gmail.com>
2023-04-03 22:04:20 +0300

    assignment 1 finished

--- assignment_1.py
+++ assignment_1.py
@@ -1,12 +1,11 @@
-import time 
 # Task 1
-num = input("choose excercice too activate : ")
+num = input()
 if num == '1a':
     # write your answer for question 1a below
     def ex_1a():
-        a = int(input("enter parameter a:"))
-        b = int(input("enter parameter b:"))
-        c = int(input("enter parameter c:"))
+        a = int(input())
+        b = int(input())
+        c = int(input())
         if (a**2 + b**2 == c**2):
             print("yes")
         else:
@@ -14,11 +13,12 @@ if num == '1a':
         # todo - optional code...
         # out = "yes" if a**2 + b**2 == c**2 else "no"
         # print(out)
+    ex_1a()
 
 elif num == '1b':
     # write your answer for question 1b below
     def ex_1b():
-        n = int(input("enter desired integer : "))
+        n = int(input())
         for a in range(2,n+1):
                 for b in range(a,n+1):
                     for c in range(b,n+1):
@@ -26,30 +26,33 @@ elif num == '1b':
                         if output : 
                             [a,b,c] = sorted([a,b,c])
                             print(a,b,c)
+    ex_1b()
 
 elif num == '1c':
     # write your answer for question 1c below
-    def ex_3a():
+    def ex_1c():
         a = int(input("enter parameter a : "))
         b = int(input("enter parameter b : "))
         pyth = (a**2 + b**2)**0.5
         output = "yes" if pyth == int(pyth) else "no"
         print(output)
+    ex_1c()
 
 elif num == '2a':
     # write your answer for question 2a below
     def ex_2a():
-        a0 = int(input("enter parameter a0: "))
-        n = int(input("enter parameter n: "))
-        d = int(input("enter parameter d: "))
+        a0 = int(input())
+        n = int(input())
+        d = int(input())
         result = 1
-        print("starting at: " + str(a0-a0%d + d))
+        # print("starting at: " + str(a0-a0%d + d))
         
         for i in range(a0 +d -a0%d,n+1,d):
             result *= i
-            print("result: " + str(result))
-            print("i:"+str(i))
+            # print("result: " + str(result))
+            # print("i:"+str(i))
         print (result)
+    ex_2a()
 
 elif num == '2b':
     # write your answer for question 2b below
@@ -59,20 +62,21 @@ elif num == '2b':
             and
             (x**2 +(n-x+1)**2 ) %m == 0
         )
-        print(x,condition)
+        # print(x,condition)
         if condition :
             return x**2 +(n-x+1)**2 
         else:
             return 1    
 
     def ex_2b():
-        n = int(input("enter parameter n: "))
-        m = int(input("enter parameter m: "))
+        n = int(input())
+        m = int(input())
         result = 1
         for i in range(1,n):
-            print(A(i,n,m))
+            # print(A(i,n,m))
             result *= A(i,n,m)
         print(result)
+    ex_2b()
 
 elif num == '2c':
     # write your answer for question 3 below
@@ -99,26 +103,27 @@ elif num == '2c':
         return factors 
 
     def ex_2c() : 
-        n = int(input("choose parameter n : "))
-        m = int(input("choose parameter m : "))
+        n = int(input())
+        m = int(input())
         
         factors = prime_fact(n)
         total = 1
         for [p,x] in factors :
-            print("prime: "+str(p),"occurences: "+str(x))
+            # print("prime: "+str(p),"occurences: "+str(x))
             if x%m==0 :
                 total *= p**x
             else:
                 total *= p*x
-            print("total: "+ str(total))
+            # print("total: "+ str(total))
         
         print("total: "+ str(total))
+    ex_2c()
 
 elif num == '3':
     # write your answer for question 3 below
     def ex_3():
-        a = int(input("choose parameter a: "))
-        b = int(input("choose parameter b: "))
+        a = int(input())
+        b = int(input())
 
         if b%a==0 :
             # if a==2 : return True
@@ -133,10 +138,9 @@ elif num == '3':
 
 elif num == '4a':
     # write your answer for question 4a below
-    print("ex_4a")
     def ex_4a():
-        s = input("enter string as s : ")
-        d = int(input("enter integer as d : "))
+        s = input()
+        d = int(input())
         result = ''
         for i in range(0,len(s)-len(s)%d,d):
             result += s[i:i+1]
@@ -146,15 +150,15 @@ elif num == '4a':
 elif num == '4b':
     # write your answer for question 4b below
     def ex_4b():
-        s1 =" "+ input("enter string input for s1 : ") +" "
-        s2 = input("enter string input for s2 : ")
-        s3 = input("enter string input for s3 : ")
+        s1 =" "+ input() +" "
+        s2 = input()
+        s3 = input()
 
         res = ''
         index_lst = []
 
         for i in range(1,len(s1)):
-            print(i)
+            # print(i)
             if (s1[i] in s2) and ((s1[i+1] in s3) or (s1[i-1] in s3)): 
                 index_lst.append(i)
 
@@ -165,70 +169,60 @@ elif num == '4b':
     ex_4b()
 
 elif num == '4c':
-    # write your answer for question 4c below
+    # write your answer for question 4b below
     def exec_a(phrase:str)->str:
-        result =''
-        for char in phrase:
-            if char in ["a","e","i","o","u","A","E","I","O","U"]:
-                result += "*"+char+"*"
-            else: result+=char
-        print("a: "+result)
-        return result
-    
-    def exec_b(phrase:str)->str:
-        if len(phrase)%2==1 : phrase = phrase +" "
+        unique_chars = ["a","e","i","o","u","A","E","I","O","U"]
         result = ''
+        for i in range(len(phrase)):
+            char = phrase[i]
+            if char in unique_chars:
+                result += "*"+char+"*"
+            else:
+                result+=char
+        # print("a: "+ result)
+        return result
 
-        for i in range(1,len(phrase),2):
-            result += phrase[i-1].lower() + phrase[i].upper()
-        
-        print("b: "+result)
-        return result.strip()
+    def exec_b(phrase:str)->str:
+        words = phrase.split()
+        result =''
+        for i in range(len(words)):
+            if i%2==1:
+                result +=" "+words[i].upper()
+            else:
+                result +=" "+words[i].lower()
+        # print("b: " +result.lstrip())
+        return result.lstrip()
 
     def exec_c(phrase:str)->str:
-        word_lst = []
-        temp = 0
-        for i in range(len(phrase)):
-            if phrase[i] == " ":
-                word_lst.append(phrase[temp:i])
-                temp = i
-            elif i == len(phrase): word_lst.append(phrase[temp:])
-        word_lst.sort()
-        for word in word_lst:
-            
+        words = phrase.split()
+        words.sort(reverse=True)
+        result = ''
+        for word in words:
+            result += " "+ word
+        # print("c: "+ result.lstrip())
+        return result.lstrip()
 
-
-    def ex_4c():
-        s = input("enter a string for parameter s: ")
+    def exec_4c():
+        phrase = input("enter phrase for parameter s: ")
+        test = phrase.split(", ")
+        output = ''
         phrase_lst = []
-        temp = 0
 
-        for i in range(len(s)):
-            if s[i:i+2] == ", " :
-                phrase_lst.append(s[temp:i])
-                temp = i
-            elif i == len(s)-2:
-                phrase_lst.append(s[temp+2:])
-        print(phrase_lst)
-        
-        for i in range(len(phrase_lst)):
-            phrase_lst[i] = exec_a(phrase_lst[i])
-            phrase_lst[i] = exec_b(phrase_lst[i])
+        for sub in test:
+            sub = exec_a(sub)
+            sub = exec_b(sub)
+            sub = exec_c(sub)
+            # print("")
+            phrase_lst.append((len(sub),sub))
+        phrase_lst.sort(key=list[0])
 
-        print(phrase_lst)
+        for phrase in phrase_lst:
+            output+=  phrase[1] + ", " 
 
-    ex_4c()
-
-
+        # print(phrase_lst)
+        print(output.lstrip()[:-2])
+        return output.lstrip()[:-2]
+    exec_4c()
 
 else:
     print('error')
-
-
-
-
-
-
-# start = time.time()
-# end = time.time()
-# print(end - start)
