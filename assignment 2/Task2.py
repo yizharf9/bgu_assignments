e89d894190f10b7c06ba3fdb0b95824728ba0244
yizhar fahima <yizharf9@gmail.com>
2023-04-09 19:58:18 +0300

    all functions are tested and fully functional

--- assignment 2/Task2.py
+++ assignment 2/Task2.py
@@ -1,4 +1,15 @@
 import datetime
+
+'''
+message format:
+msg_i=[
+	id:int,
+	username:str,
+	content:str,
+	time_of_send:str(time)
+]
+'''
+
 #region
 def current_time_to_str():
     time = datetime.datetime.now()
@@ -18,63 +29,115 @@ def msg_to_string(msg):
 	msg_str = "("+str(msg[0])+") "+msg[3]+" "+msg[1]+": "+msg[2]+"\n"
 	return msg_str
 
-def conversation_to_string(conversation):
+def conversation_to_string(conversation:list)->str:
 	#TODO
     output = ''
-    conv = show_conversation(conversation)
-    for msg in conv:
-        output += msg
-	
-    return output
+    for msg in conversation:
+        output += msg_to_string(msg)
+    return output[:-1]
 
 def show_conversation(conversation):
-	#TODO
 	print(conversation_to_string(conversation))
 
 def send_msg(username:str, msg_content:str, msg_last_id:int, conversation_size:int, max_conversation_size:int, conversation:list):
 	#TODO
-    if enough_space(msg_content,conversation_size,max_conversation_size):
-        conversation_size +=1
-        msg_last_id = conversation_size
-	
-        new_msg = [msg_last_id,username,msg_content,current_time_to_str()]
-        conversation.append(new_msg)
-        print("Message was sent successfully!")
-    else:
-        print("There is not enough space in the storage!")
-    return msg_last_id,conversation_size
+	if enough_space(msg_content,conversation_size,max_conversation_size):
+		conversation_size += len(msg_content)
+		msg_last_id += 1
+		new_msg = [msg_last_id,username,msg_content,current_time_to_str()]
+		conversation.append(new_msg)
+		print(conversation_size)
+	else:
+		print("There is not enough space in the storage!")
+	return msg_last_id,conversation_size
 
 def find_msg_index(msg_id, conversation):
-	#TODO
-    for i,msg in enumerate(conversation):
-        if msg[1] == msg_id:
-            return conversation.index(msg)
-        else:
-            return -1
-	
+	#TODO 
+	for i,msg in enumerate(conversation):
+		if msg[0] == msg_id:
+			return i
+	return -1
+    
 def delete_msg(msg_id, conversation_size, conversation):
 	#TODO
-    
-	return
+	index = find_msg_index(msg_id,conversation)
+	if index != -1 :
+		deleted_msg = conversation[index]
+		conversation_size = conversation_size - len(deleted_msg)
+		conversation.pop(index)
+		return conversation_size
+	else:
+		return -1
 
 def star_marking(msg_id, conversation):
 	#TODO
-	return
-		
+	index = find_msg_index(msg_id,conversation)
+	if index!=-1:
+		if len(conversation[index]) == 4 :
+			conversation[index].append('Starred')
+		else:
+			conversation[index].pop(4)
+		return 0 
+	else:
+		return -1
+
 def print_starred_messages(conversation):
 	#TODO
-	return
+	output = ''
+	for msg in conversation:
+		if (len(msg) == 5) and (msg[4] == "Starred"):
+			output += msg_to_string(msg)
+	if len(output) == 0:
+		return -1
+	else: 
+		return output[:-1]
 
-def file_fixing(filename):
+def file_fixing(filename:str):
 	#TODO
-	return
+	if filename.endswith('.txt') :
+		fh = open(filename)
+		output = ''
+		for char in fh.read():
+			output = char + output
 
-def internal_check(conversation):
+def count_chars(full_txt:str):
+	char_count:dict[str,int] = {}
+	for char in full_txt:
+		ascii_i = ord(char.upper())
+		if ascii_i>=65 and ascii_i<=90 :
+			char_count[char.upper()] = char_count.get(char.upper(),0)+1
+	sorted_char_count = sorted(char_count,key=lambda key : char_count[key],reverse=True)
+	return(sorted_char_count[0],char_count[sorted_char_count[0]])
+
+def contains_abc(conversation:list)->int:
+	count = 0
+	for msg in conversation:
+		contains_all = True
+		msg_txt = msg[2] 
+		for i in range(65,91):
+			if (not chr(i).lower() in msg_txt) and (not chr(i).upper() in msg_txt) :
+				contains_all = False
+		if contains_all : count += 1
+	return count
+
+def internal_check(conversation:list):
 	#TODO
-	return
+	full_abc = contains_abc(conversation)
+	
+	full_txt = ''
+	for msg in conversation:
+		msg_txt = msg[2]
+		full_txt+= msg_txt
+	
+	(freq_char,num_of_times) = count_chars(full_txt)
+	
+
+	return (full_abc,freq_char,num_of_times)
+
+
 #endregion
 
-def interactive_system(conversation_members = ('yizhar',"Steve", "Bill"), max_conversation_size = 300):
+def interactive_system(conversation_members = ("yizhar","Steve", "Bill"), max_conversation_size = 300):
 	conversation_size = 0
 	conversation = []
 	msg_last_id = 0
