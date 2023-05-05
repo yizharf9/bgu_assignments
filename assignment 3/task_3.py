b9c80b4c9f70f7a72f3aca8a2786592de2322644
yizhar fahima <yizharf9@gmail.com>
2023-05-05 16:52:13 +0300

    assignment 3: k_size_subset() not ready, being redone dict_depth() finished

--- assignment 3/task_3.py
+++ assignment 3/task_3.py
@@ -1,4 +1,4 @@
-def linear_sum(x:list, result:int):    
+def linear_sum(x:list, result:int) -> bool:    
     # base case - the list is size 0,
     # setting the result to 0 means that we found a linear sum
     # else, we return to different starting conditions
@@ -7,7 +7,6 @@ def linear_sum(x:list, result:int):
     b = 0
 
     if len(x)> 1: b = x[1]
-    # 
     if (
         a == result or
         a + b == result or
@@ -28,35 +27,87 @@ def linear_sum(x:list, result:int):
 # endregion
 
 
-def ordered_subset(str1:str, str2:str):
+def ordered_subset(str1:str, str2:str) -> bool:
     # base case - comparing 2 chars
-    if str2 == '' : return True
-    elif str1 == '' and len(str2) !=0 :return False
+    # ! second condition - 2 consequtive chars of str2 in str1 returns false 
+    if str1[0:2] == str2[0:2] : 
+        return False
+    # ! first condition - str2 is empty means the prev chars of str2 are in str1 => is a subset...
+    if str2 == '' : 
+        return True
+    # in case that we went through all the chars in str1 and still have some in str2
+    # means that not all chars in str2 appear in str1
+    elif str1 == '' and len(str2) !=0 : 
+        return False
+    
     print(str1,str2)
+
     if str1[0] == str2[0]:
         return ordered_subset(str1[1:],str2[1:]) 
     return ordered_subset(str1[1:],str2)
 
-
 # region - testing
 #        ↓  ↓
-str1 = "aetgwae"
-str2 = "ewf"
-answer = ordered_subset(str1,str2)
-print(answer)
+# str1 = "aetgwlae"
+# str2 = "ewl"
+# str1 = "ladbxcfe"
+# str2 = "abc"
+# answer = ordered_subset(str1,str2)
+# print(answer)
+# endregion
+
+def n_set(n:int)->str:
+    if n == 0 :
+        return ''
+    return  n_set(n-1) + str(n) 
+
+
+
+
+def k_size_subsets(n:int, k:int) -> str:
+    # base case - n==k
+    if n == k : 
+        return [n_set(n)]
+    if n > k :
+        n-=1
+        
+    pass
+    
+
+# region - testing
+# answer = n_set(5)
+# answer = k_size_subsets(5,3)
+# [‘123’, ‘124’, ‘125’, ‘134’, ‘135’, ‘145’, ‘234’, ‘235’, ‘245’, ‘345’]
+# print(answer)
 # endregion
 
 
+def dict_depth(d:dict)->int:
 
-def k_size_subsets(n, k):
-    # Delete the pass command and insert you code below
-    pass
+    if type(d) != dict:
+        return -1
+    elif type(d) == dict:
+        max = 0
+        for key in  d.keys():
+            res =  dict_depth(d.get(key,0)) + 1
+            if res > max : max = res
+        return max
+        
 
+# region - testing
 
-def dict_depth(d):
-    # Delete the pass command and insert you code below
-    pass
-
+# depth = 0
+test0 = {}
+# depth = 1
+test1 = {1: {1: "a", 2: "b"}, 2: "b"}
+# depth = 2
+test2 = {1: {1: "a", 2:"b"}, 2: {1: {1: "a", 2: "b"}, 2: "b"}}
+# depth = 2
+test3 = {1: {1: "a",2: "b"},4: {1:{1: "a",2: "b"},2: "b"},3: {1: "a",2: "b"},2: "b"}
+arr = [test0,test1,test2,test3]
+# for test in arr:
+#     print(dict_depth(test))
+# endregion
 
 def nested_get(d, key):
     # Delete the pass command and insert you code below
