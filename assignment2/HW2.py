d856f8e683990395ec5a09f9f331bb87d8bda53f
yizhar fahima <yizharf9@gmail.com>
2023-05-11 01:41:11 +0300

    assignment 3: all functions finished 4 a,b,c - not tested yet

---
+++ assignment2/HW2.py
@@ -0,0 +1,210 @@
+import datetime
+
+'''
+message format:
+msg_i=[
+	id:int,
+	username:str,
+	content:str,
+	time_of_send:str(time)
+]
+'''
+
+#region
+def current_time_to_str():
+    time = datetime.datetime.now()
+    return time.strftime("%H:%M:%S")
+
+def is_member(username, conversation_members):
+	return username in conversation_members
+
+def enough_space(msg_content:str, conversation_size:int, max_conversation_size:int)->bool:
+	#TODO
+	return len(msg_content) + conversation_size <= max_conversation_size
+
+def conversation_is_empty(conversation):
+	return len(conversation) == 0
+
+def msg_to_string(msg):
+	msg_str = "("+str(msg[0])+") "+msg[3]+" "+msg[1]+": "+msg[2]+"\n"
+	return msg_str
+
+def conversation_to_string(conversation:list)->str:
+	#TODO
+    output = ''
+    for msg in conversation:
+        output += msg_to_string(msg)
+    return output[:-1]
+
+def show_conversation(conversation):
+	print(conversation_to_string(conversation))
+
+def send_msg(username:str, msg_content:str, msg_last_id:int, conversation_size:int, max_conversation_size:int, conversation:list):
+	#TODO
+	if enough_space(msg_content,conversation_size,max_conversation_size):
+		if msg_content.endswith(".txt"):
+			file_fixing(msg_content)
+		conversation_size += len(msg_content)
+		msg_last_id += 1
+		new_msg = [msg_last_id,username,msg_content,current_time_to_str()]
+		conversation.append(new_msg)
+	return msg_last_id,conversation_size
+
+def find_msg_index(msg_id, conversation):
+	#TODO 
+	for i,msg in enumerate(conversation):
+		if msg[0] == msg_id:
+			return i
+	return -1
+    
+def delete_msg(msg_id, conversation_size, conversation):
+	#TODO
+	index = find_msg_index(msg_id,conversation)
+	if index != -1 :
+		deleted_msg = conversation[index]
+		conversation_size = conversation_size - len(deleted_msg)
+		conversation.pop(index)
+		return conversation_size
+	else:
+		return -1
+
+def star_marking(msg_id, conversation):
+	#TODO
+	index = find_msg_index(msg_id,conversation)
+	if index!=-1:
+		if len(conversation[index]) == 4 :
+			conversation[index].append('Starred')
+		else:
+			conversation[index].pop(4)
+		return 0 
+	else:
+		return -1
+
+def print_starred_messages(conversation):
+	#TODO
+	output = ''
+	for msg in conversation:
+		if (len(msg) == 5) and (msg[4] == "Starred"):
+			output += msg_to_string(msg)
+	if len(output) == 0:
+		return -1
+	else: 
+		return output[:-1]
+
+def file_fixing(filename:str):
+    output = ''
+    with open(filename) as f :
+        content = f.readlines()
+        for line in content:
+            newline = ''
+            word_lst = line.split()
+            for word in word_lst :
+                back_word = ''
+                for i in range(1,len(word)+1):
+                    back_word += word[-i]
+                newline+= back_word +" "
+            output += newline.lstrip() +'\n'
+    with open('output_'+filename[2:],'w') as f :
+        f.write(output) 
+
+
+def count_chars(full_txt:str):
+	char_count:dict[str,int] = {}
+	def get_value(k:str)->int: return char_count[k]
+
+	for char in full_txt:
+		ascii_i = ord(char.upper())
+		if ascii_i>=65 and ascii_i<=90 :
+			char_count[char.upper()] = char_count.get(char.upper(),0)+1
+	# sorted_char_count = sorted(char_count,key=lambda key : char_count[key],reverse=True)
+	sorted_char_count = sorted(char_count,key= get_value,reverse=True)
+	return(sorted_char_count[0].lower(),char_count[sorted_char_count[0]])
+
+def contains_abc(conversation:list)->int:
+	count = 0
+	for msg in conversation:
+		contains_all = True
+		msg_txt = msg[2] 
+		for i in range(65,91):
+			if (not chr(i).lower() in msg_txt) and (not chr(i).upper() in msg_txt) :
+				contains_all = False
+		if contains_all : count += 1
+	return count
+
+def internal_check(conversation:list):
+	#TODO
+	full_abc = contains_abc(conversation)
+	
+	full_txt = ''
+	for msg in conversation:
+		msg_txt = msg[2]
+		full_txt+= msg_txt
+	
+	(freq_char,num_of_times) = count_chars(full_txt)
+	
+
+	return (full_abc,freq_char,num_of_times)
+
+
+#endregion
+
+def interactive_system(conversation_members = ("yizhar","Steve", "Bill"), max_conversation_size = 300):
+	conversation_size = 0
+	conversation = []
+	msg_last_id = 0
+	while True:
+		print("##################################################\nWelcome to UpWhats! What would you like to do?\n\
+[0] End conversation\n[1] Show full conversation\n[2] Send new message\n[3] Remove existing message\n\
+[4] Star a message\n[5] Show starred messages\n[6] Internal check")
+		username = input("Please enter username (only conversation's members are allowed to send/read messages.)\n")
+		if is_member(username, conversation_members):
+			choice = input("Please type your choice and press ENTER\n")
+			if choice not in ["0", "1", "2", "3", "4", "5", "6"]:
+				print("Invalid choice")
+				continue
+			if choice == '0':
+				print("Thank you for using UpWhats! See you soon. Bye.")
+				break
+			elif choice == '1':
+				if conversation_is_empty(conversation):
+					print("Conversation is empty")
+				else:
+					show_conversation(conversation)
+			elif choice == '2':
+				msg_content = input("Please type your message.\n")
+				prev_msg_last_id, prev_conversation_size = msg_last_id, conversation_size
+				msg_last_id, conversation_size = send_msg(username, msg_content, msg_last_id, conversation_size, max_conversation_size, conversation)
+				if prev_msg_last_id == msg_last_id and prev_conversation_size == conversation_size:
+					print("There is not enough space in the storage!")
+				else:
+					print("Message was sent successfully!")
+			elif choice == '3':
+				msg_id = input("Please enter message id.\n")
+				new_size = delete_msg(int(msg_id), conversation_size, conversation)
+				if new_size == -1:
+					print("There is no message with this identifier")
+				else:
+					conversation_size = new_size
+					print("Message was removed successfully!")
+			elif choice == '4':
+				msg_id = input("Please enter message id.\n")
+				marked = star_marking(int(msg_id), conversation)
+				if marked == -1:
+					print("There is no message with this identifier")
+			elif choice == '5':
+				starred_concat = print_starred_messages(conversation)
+				if starred_concat == -1:
+					print("There are no starred messages")
+				else:
+					print(starred_concat)
+			elif choice == '6':
+				internal_check_tuple = internal_check(conversation)
+				print("The number of messages that contains all the alphabet is {0}, and the most common char is '{1}' \
+which appeared {2} times".format(str(internal_check_tuple[0]), internal_check_tuple[1], internal_check_tuple[2]))
+		else:
+			print("You're not a member of this group!")
+
+if __name__ == "__main__":
+    interactive_system()
+    
+
---
+++ assignment3/task_3.py
@@ -0,0 +1,123 @@
+# aux
+# 1
+def linear_sum(x:list, result:int) -> bool:    
+    # base case - the list is size 0,
+    # setting the result to 0 means that we found a linear sum
+    # else, we return to different starting conditions
+    if len(x) == 0 : return result == 0
+    a = x[0]
+    b = 0
+
+    if len(x)> 1: b = x[1]
+    if (
+        a == result or
+        a + b == result or
+        a - b == result
+    ): return True
+    # vars representing the reduced problem for the next rec...
+    temp = x[0]
+    new_x = x[1:]
+
+    if linear_sum(new_x,result-temp):return True
+    elif linear_sum(new_x,result+temp):return True
+    return linear_sum(new_x,result)
+# 2a
+def ordered_subset(str1:str, str2:str) -> bool:
+    # base case - comparing 2 chars
+    # ! second condition - 2 consequtive chars of str2 in str1 returns false 
+    if str1[0:2] == str2[0:2] : 
+        return False
+    # ! first condition - str2 is empty means the prev chars of str2 are in str1 => is a subset...
+    if str2 == '' : 
+        return True
+    # in case that we went through all the chars in str1 and still have some in str2
+    # means that not all chars in str2 appear in str1
+    elif str1 == '' and len(str2) !=0 : 
+        return False
+    
+    print(str1,str2)
+
+    if str1[0] == str2[0]:
+        return ordered_subset(str1[1:],str2[1:]) 
+    return ordered_subset(str1[1:],str2)
+# 2b
+def n_set(n:int)->str:
+    if n == 0 :
+        return ''
+    else : 
+        return n_set(n-1) + str(n)
+
+def rec_sub(k:int,subsets:list[str],subset:str,i:int=0)->None:
+    # base case - subset len(k)...
+    if len(subset) == k :
+        subsets.append(subset)
+    # rec step - 
+    elif i > k:
+        pass
+    else:
+        rec_sub(k,subsets,subset[:i]+subset[i+1:],i)
+        rec_sub(k,subsets,subset,i+1)
+
+def k_size_subsets(n:int, k:int) -> list[str]:
+    a = []
+    rec_sub(k,a,n_set(n))
+    return a
+# 3a
+def recurse_depth(d:dict)->int:
+    max = 0
+    if type(d) != dict :
+        max -= 1
+    elif type(d) == dict:
+        for key in  d.keys():
+            #! recursive call
+            res =  recurse_depth(d.get(key,0)) + 1
+            if res > max : 
+                max = res
+    return max    
+
+def dict_depth(d:dict):
+    if type(d) != dict :
+        return TypeError('not a dictionary')
+    return recurse_depth(d)
+# 3b
+def nested_get(d:dict, key:int)->list[str]:
+    res = []
+    # base case - a dict with the wanted key
+    if type(d.get(key,0)) == str : 
+        res.append(d.get(key))
+    for sub in d.keys():
+        if type(d.get(sub,0)) == dict :
+            res += nested_get(d[sub],key)
+    return res
+# 4a
+def distance(row1, col1, row2, col2):
+    return abs(row1-row2) + abs(col1-col2)
+
+# 4b
+def add_tower(board:list[int], d:int, row:int, col:int):
+    for _row,_col in enumerate(board[:row]):
+        # print("distance :" + str(distance(row,_row,col,_col)))
+        if distance(row,col,_row,_col) <= d :
+            return False
+    board[row] = col
+    return True
+
+
+def rec(board:list[int], d:int, i=0):
+    n = len(board)
+    if i == n-1 : 
+        for j in range(n-1):
+            if add_tower(board,d,n-1,j):
+                return True
+    # for j in range(n-1):
+    if add_tower(board,d,i,(board[i-1]+d)%n) and rec(board,d,i+1) :
+            return True
+
+
+# 4c
+def n_towers(n:int,d:int)->list[int]:
+    n_board= n*[0]
+    if n>1 and d>=n**0.5 : 
+        return []
+    rec(n_board,d)
+    return n_board
