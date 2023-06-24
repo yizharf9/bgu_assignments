f5dd9f29d1160d8e7283a57af0de20ca15fe9d1c
yizhar fahima <yizharf9@gmail.com>
2023-06-24 21:27:27 +0300

    assignment 5: files renamed

--- assignment5/test.py
+++ assignment5/test.py
@@ -1,60 +1,17 @@
-
 import numpy as np
+import pandas as pd
 from matplotlib import pyplot as plt
-def y_i(N,f,init,h):
-    if N == 0:
-        return init
-    else:
-        y_i_1 = y_i(N-1,f,init,h)
-        t_i_1 = (N-1)*h
-        return y_i_1 + h * f(t_i_1,y_i_1)
+from sklearn.cluster import KMeans
+from q1_template import euler,plot_funcs
+from q2_template import gaussian
+from q3_template import import_to_numpy,import_to_pandas
+path = "./nci.data.csv"
 
+df = import_to_pandas(path)
+labels = import_to_numpy("./nci_labels.txt")
+data = df.to_numpy()
+data = data[:,1:]
+data.astype("float")
 
+s1,s2= df["s1"],df["s2"]
 
-def euler(f, a: float, b: float, initial_cond: float, N=None, h=None):
-    # input checking block
-    if N == None and h == None:
-        raise ValueError("please fill at least one of the required areas: N, h")
-    elif N != None and h != None and (b - a) / N != h:
-        raise ValueError(
-            f"make sure the parameters h and N are mathcing by value \n expected val for h:{h} , val for h:{(b-a)/N}"
-        )
-    elif N != None:
-        h = (b - a) / N
-    elif h != None:
-        N = int((b - a) / h)
-
-    # arrays to keep data
-    arr_t = np.zeros(N+1)
-    arr_y = np.zeros(N+1)
-
-    # setting values of array
-    arr_y[0] = initial_cond
-    arr_t[0] = 0
-
-    for i in range(N):
-        t_i_1 = arr_t[i]
-        y_i_1 = arr_y[i]
-
-        arr_t[i] = t_i_1
-        if i<N:
-            arr_y[i+1] = y_i_1 + h * f(t_i_1,y_i_1) 
-            arr_t[i+1] = t_i_1 + h 
-        # print(y_i_1,y_i,t_i_1,h)
-
-    return arr_t,arr_y
-
-
-f = lambda t,y : y - t**2 +1
-init =  0.5
-a = 0
-b = 2
-N = 10
-h = (b-a)/N
-
-res = euler(f,a,b,init,N)
-print(res[0])
-print(res[1])
-
-print(.5+0.2*(.5-.2**2+1))
-print(init+h*f(0.2,init))
